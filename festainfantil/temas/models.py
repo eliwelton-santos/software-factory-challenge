8afaeb36d127db4e78390d7060e0c3e648efc8dd
EliweltonMoreira <eliwelton.moreira@outlook.com>
2020-09-11 23:22:16 -0300

    Cria funcionalidade de listar os temas cadastrados no banco

--- festainfantil/temas/models.py
+++ festainfantil/temas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Tema(models.Model):
+
+    COR = (
+        ('Vermelho', 'Vermelho'),
+        ('Verde', 'Verde'),
+        ('Azul', 'Azul'),
+        ('Amarelo', 'Amarelo'),
+        ('Magenta', 'Magenta'),
+        ('Ciano', 'Ciano'),
+        ('Branco', 'Branco'),
+        ('Preto', 'Preto'),
+    )
+
+    nome = models.CharField(max_length=30)
+    valorAluguel = models.DecimalField(max_digits=6, decimal_places=2)
+    corDestaque = models.CharField(max_length=20, choices=COR)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
--- festainfantil/temas/views.py
+++ festainfantil/temas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Tema
 
 
 def index(request):
-    return render(request, 'temas/index.html')
+    temas = Tema.objects.all().order_by('-created_at')
+    return render(request, 'temas/index.html', {'temas': temas})
