2a40560bda0b9c3ae158c0d08f68b14ff40afc3e
EliweltonMoreira <eliwelton.moreira@outlook.com>
2020-09-12 00:02:11 -0300

    Cria funcionalidade de alterar os campos dos temas cadastrados

--- festainfantil/temas/urls.py
+++ festainfantil/temas/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='tema_index'),
     path('adicionar/', views.adicionar, name='tema_adicionar'),
+    path('editar/<int:id>', views.editar, name='tema_editar'),
 ]
--- festainfantil/temas/views.py
+++ festainfantil/temas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Tema
 from .forms import TemaForm
@@ -22,3 +22,19 @@ def adicionar(request):
     else:
         form = TemaForm()
         return render(request, 'temas/adicionar.html', {'form': form})
+
+
+def editar(request, id):
+    tema = get_object_or_404(Tema, pk=id)
+    form = TemaForm(instance=tema)
+    if request.method == 'POST':
+        form = TemaForm(request.POST, instance=tema)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Tema atualizado com sucesso!')
+            return redirect('tema_index')
+        else:
+            messages.warning(request, 'Um ou mais campos foram preenchidos incorretamente!')
+            return render(request, 'temas/editar.html', {'form': form, 'tema': tema})
+    else:
+        return render(request, 'temas/editar.html', {'form': form, 'tema': tema})
