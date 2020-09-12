85521b1e0aa1050aa6125586b6e9fa51a7eee6b7
EliweltonMoreira <eliwelton.moreira@outlook.com>
2020-09-11 22:14:04 -0300

    Configura banco de dados

--- festainfantil/festainfantil/settings.py
+++ festainfantil/festainfantil/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'festainfantil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'festainfantil',
+        'USER': 'postgres',
+        'PASSWORD': 'trick69',
     }
 }
 
